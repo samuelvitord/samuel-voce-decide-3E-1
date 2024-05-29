31829776bc51c213cda57be85297818da6e9759f
samuelvitord <167381451+samuelvitord@users.noreply.github.com>
2024-05-29 23:47:17 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,48 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    
+    enunciado: "Pergunta 1",
+    alternativas : [
+  "Resposta 1"
+  "Resposta 2"
+  },
+
+  {
+
+    enunciado: "Pergunta 2",
+    alternativas : [
+  "Resposta 1"
+  "Resposta 2"
+  },  
+
+  {
+
+    enunciado: "Pergunta 3",
+    alternativas : [
+  "Resposta 1"
+  "Resposta 2"
+  },
+
+  {
+
+    enunciado: "Pergunta 4",
+    alternativas : [
+  "Resposta 1"
+  "Resposta 2"
+  },
+
+  {
+
+    enunciado: "Pergunta 5",
+    alternativas : [
+  "Resposta 1"
+  "Resposta 2"
+  },
+];
+
